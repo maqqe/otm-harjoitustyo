be54e8614826a65d8b6ce950eae52d95b904f093
maqqe <mkeskitalo@gmail.com>
2018-03-21 17:49:10 +0200

    viikko2 toinen kohta tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLisaysKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        kortti.otaRahaa(9);
+        assertEquals("saldo: 0.01", kortti.toString());
+        // Tästä testistä tulee hylsy, koska maksukorttiluokan toString on "väärin".
+        // Saldo vähenee aivan oikein yhteen senttiin, mutta toString metodi on luotu siten,
+        // että tässä saa kuvan, että saldoa on itseasiassa 10 senttiä.
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaaTarpeeksi() {
+        assertTrue(kortti.otaRahaa(10));
+    } 
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahaaLiianVahan() {
+        assertFalse(kortti.otaRahaa(1000));
+    }
+    
 }
